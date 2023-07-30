3586023b8e2f982ceea09236de1f71d6fc3c590b
rubberplayer <rubber.player@gmx.fr>
2023-07-30 17:43:36 +0200

    cleanup

--- src/player.h
+++ src/player.h
@@ -8,18 +8,20 @@ class Player
 public:
   Player();
   ~Player();
+  
   pa_simple *s;
   pa_sample_spec ss;
+  
   void connect_to_pulseaudio();
   void play_some();
   void play_always();
   void play_some(float *ptr, size_t bytes);
   std::thread the_play_thread;
+  std::atomic<bool> terminate_the_play_thread;
+  
   void start_playing();
   void stop_playing();
-
   std::atomic<bool> play_started;
-  std::atomic<bool> terminate_the_play_thread;
-
+  
   RubberBand::RubberBandStretcher *rubberBandStretcher;
 };
