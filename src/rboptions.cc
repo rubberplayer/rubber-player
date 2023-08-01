9b0f0d0f8807fde066204245cceeb09bb853e9e1
rubberplayer <rubber.player@gmx.fr>
2023-08-02 00:01:43 +0200

    rubberband options ui 1.01/n

--- src/rboptions.cc
+++ src/rboptions.cc
@@ -26,10 +26,17 @@ RubberBandOptionsWindow::RubberBandOptionsWindow()
         RubberBandOption("Channels", {"Apart", "Together"}, {"R2", "R3"}),
     };
 
+    //std::map<RubberBandOption, Gtk::DropDown> drop_downs;
+    //std::list<Gtk::DropDown&> drop_downs;
+
     this->set_default_size(220, 100);
     this->set_title("Stretching");
     this->set_resizable(false);
     set_child(m_vertical_box);
+    m_vertical_box.set_margin_start(10);
+    m_vertical_box.set_margin_end(10);
+    m_vertical_box.set_margin_top(10);
+    m_vertical_box.set_margin_bottom(10);
 
     for (auto option : optionss)
     {
@@ -37,10 +44,6 @@ RubberBandOptionsWindow::RubberBandOptionsWindow()
         auto option_horizontal_box = Gtk::Box();
         option_horizontal_box.set_hexpand();
         m_vertical_box.append(option_horizontal_box);
-        m_vertical_box.set_margin_start(10);
-        m_vertical_box.set_margin_end(10);
-        m_vertical_box.set_margin_top(10);
-        m_vertical_box.set_margin_bottom(10);
 
         // label
         auto option_label = Gtk::Label();
@@ -64,17 +67,17 @@ RubberBandOptionsWindow::RubberBandOptionsWindow()
         auto model = Gtk::StringList::create(strings);
         option_dropdown.set_model(model);
         option_horizontal_box.append(option_dropdown);
-        //option_dropdown.set_selected(1);
-        //printf("option_dropdown sele %d\n",option_dropdown.get_selected());
-/*        auto connection = option_dropdown.property_selected_item().signal_changed().connect(
-            sigc::bind(
-                sigc::mem_fun(
-                    *this,
-                    &RubberBandOptionsWindow::selected_item_changed)));
 
-        
-        */
+     //   drop_downs.emplace(std::pair{option,option_dropdown});
+    
+        //drop_downs[option] = option_dropdown; 
+        //drop_downs.push_back(option_dropdown);
     }
+
+    auto option_validate_button = Gtk::Button();
+    option_validate_button.set_label("Appliquer");
+    option_validate_button.set_hexpand();
+    m_vertical_box.append(option_validate_button);
 
     printf("-----\n");
 }
@@ -83,3 +86,14 @@ RubberBandOptionsWindow::RubberBandOptionsWindow()
 // {
 //     printf("pas rendu\n");
 // }
+
+//         //option_dropdown.set_selected(1);
+//         //printf("option_dropdown sele %d\n",option_dropdown.get_selected());
+// /*        auto connection = option_dropdown.property_selected_item().signal_changed().connect(
+//             sigc::bind(
+//                 sigc::mem_fun(
+//                     *this,
+//                     &RubberBandOptionsWindow::selected_item_changed)));
+//
+//
+//         */
