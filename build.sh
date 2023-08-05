ec9b9ac2831605c2cdb6b098e0828849bfcafe09
rubberplayer <rubber.player@gmx.fr>
2023-08-05 12:24:05 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 g++  -std=c++17 \
-    src/base.cc src/player.cc src/rboptions.cc src/selections.cc src/sound.cc src/waveform.cc \
+    src/base.cc src/db.cc src/player.cc src/rboptions.cc src/selections.cc src/sound.cc src/waveform.cc \
     `pkg-config --cflags --libs gtkmm-4.0` \
     `pkg-config --cflags --libs libpulse-simple` \
     `pkg-config --cflags --libs sndfile` \
     `pkg-config --cflags --libs rubberband` \
-    `pkg-config --cflags --libs jsoncpp` \
-    `pkg-config --cflags --libs sqlite3` \    
+    `pkg-config --cflags --libs sqlite3` \
     -o rubber-player
