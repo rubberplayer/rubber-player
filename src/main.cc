227c8503e260c2b6a7fa4923d0914e686164d49f
rubberplayer <rubber.player@gmx.fr>
2023-08-11 10:34:05 +0200

    only views, no unique application window

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-  auto application = ExampleApplication::create();
+  auto application = RPApplication::create();
 
   // Start the application, showing the initial window,
   // and opening extra views for any files that it is asked to open,
